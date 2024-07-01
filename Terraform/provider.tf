98f32c035e452d0dc628b9439733f4a6bbddbdf5
Himalaya7087 <157100128+Himalaya7087@users.noreply.github.com>
2024-07-01 18:20:33 +0530

    Update provider.tf

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -10,6 +10,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "ap-south-1"
-  shared_credentials_files = ["/home/ubuntu/.aws/credentials"]
-  profile = "default"
+  #shared_credentials_files = ["/home/ubuntu/.aws/credentials"]
+  #profile = "default"
 }

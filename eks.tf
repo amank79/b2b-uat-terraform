5a5bc08a8bc6d9dc0401e575e6d110be9022abe1
Himalaya7087 <himalayasingh.hs@gmail.com>
2024-12-12 20:24:42 +0530

    update eks

--- eks.tf
+++ eks.tf
@@ -4,33 +4,29 @@ module "eks" {
 
   cluster_name    = "eks-cluster"
   cluster_version = "1.30"
+  create_iam_role = true
 
-enable_irsa = true
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
 
-  bootstrap_self_managed_addons = false
+  cluster_additional_security_group_ids = ["${aws_security_group.node_sg.id}"]
+
+
+
+
   cluster_addons = {
-    coredns                = {}
     eks-pod-identity-agent = {}
-    kube-proxy             = {}
-    vpc-cni                = {}
+
   }
 
 
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.private_subnets
 
 
-  eks_managed_node_groups = {
-      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
-      ami_type       = "AL2_x86_64"
-      instance_types = ["t2.micro"]
-      vpc_security_group_ids = [aws_security_group.node_sg.id]
 
-      min_size     = 1
-      max_size     = 10
-      desired_size = 2
-    
-  }
+
 
   tags = {
     Environment = "dev"
--- iam.tf
+++ iam.tf
@@ -1,33 +1,33 @@
-resource "aws_iam_role" "Demo-App-Role" {
-  name = var.iam_name
-  
-   assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
+# resource "aws_iam_role" "Demo-App-Role" {
+#   name = var.iam_name
 
-  tags = {
-    Name = "Demo-App-IAM-Role"
-  }
-}
+#   assume_role_policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Effect": "Allow",
+#       "Principal": {
+#         "Service": "ec2.amazonaws.com"
+#       },
+#       "Action": "sts:AssumeRole"
+#     }
+#   ]
+# }
+# EOF
 
-resource "aws_iam_role_policy_attachment" "demo_app_policy" {
-  for_each = var.iam_policy_arns
-  role = aws_iam_role.Demo-App-Role.name
-  policy_arn = each.value
-}
+#   tags = {
+#     Name = "Demo-App-IAM-Role"
+#   }
+# }
 
-resource "aws_iam_instance_profile" "demo_app-Instance-Profile" {
-  name = var.iam_name
-  role = aws_iam_role.Demo-App-Role.name
-}
+# resource "aws_iam_role_policy_attachment" "demo_app_policy" {
+#   for_each   = var.iam_policy_arns
+#   role       = aws_iam_role.Demo-App-Role.name
+#   policy_arn = each.value
+# }
+
+# resource "aws_iam_instance_profile" "demo_app-Instance-Profile" {
+#   name = var.iam_name
+#   role = aws_iam_role.Demo-App-Role.name
+# }

4f73e2f1a9c083ca4a8a3f90789e8ac034b554ee
Himalaya7087 <himalayasingh.hs@gmail.com>
2024-12-14 20:59:00 +0530

    add node groups

--- eks.tf
+++ eks.tf
@@ -1,23 +1,23 @@
-# module "eks" {
-#   source  = "terraform-aws-modules/eks/aws"
-#   version = "~> 20.0"
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
 
-#   cluster_name    = "eks-cluster"
-#   cluster_version = "1.30"
-#   create_iam_role = true
+  cluster_name    = "eks-cluster"
+  cluster_version = "1.30"
+  create_iam_role = true
 
-#   cluster_endpoint_private_access = true
-#   cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
 
-#   cluster_additional_security_group_ids = ["${aws_security_group.node_sg.id}"]
-
-
+  cluster_additional_security_group_ids = ["${aws_security_group.node_sg.id}"]
 
 
-#   cluster_addons = {
-#     eks-pod-identity-agent = {}
 
-#   }
+
+  cluster_addons = {
+    eks-pod-identity-agent = {}
+
+  }
 #  eks_managed_node_group_defaults = {
 #     ami_type               = "AL2023_x86_64_STANDARD"
 #     instance_types         = ["t2.micro"]
@@ -33,16 +33,16 @@
 #     }
 #   }
 
-#   vpc_id                   = module.vpc.vpc_id
-#   subnet_ids               = module.vpc.private_subnets
-#   control_plane_subnet_ids = module.vpc.private_subnets
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.private_subnets
 
 
 
 
 
-#   tags = {
-#     Environment = "dev"
-#     Terraform   = "true"
-#   }
-# }
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}

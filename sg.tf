88b0c5d162bba86a7c47a08b0047266e82870ad3
Himalaya7087 <himalayasingh.hs@gmail.com>
2024-12-15 10:21:33 +0530

    add node groups

--- sg.tf
+++ sg.tf
@@ -27,7 +27,31 @@
 #   cidr_blocks       = var.nodes_egress_rules[count.index].cidr_blocks
 #   type              = "egress"
 # }
+resource "aws_security_group" "control_plane_sg" {
+  name        = "EKS-Control-Plane-SG"
+  description = "Security group for EKS Control Plane"
+  vpc_id      = module.vpc.vpc_id # Replace with your VPC ID or define it in variables
 
+  ingress {
+    description = "Allow SSH access"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] # Replace with a specific IP range for better security
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # -1 allows all protocols
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "EKSControlPlaneSG-${var.environment}"
+  }
+}
 
 resource "aws_security_group" "ssh_sg" {
   name        = "allow-ssh"

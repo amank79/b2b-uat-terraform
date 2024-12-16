812847624499f28ce29e579ca917fa345cb3a928
Himalaya7087 <himalayasingh.hs@gmail.com>
2024-12-16 16:25:01 +0530

    add alb-controller

--- provider.tf
+++ provider.tf
@@ -16,17 +16,29 @@ provider "aws" {
 # provider "http" {
 # }
 
-data "aws_eks_cluster" "eks_cluster" {
-  name = module.eks.cluster_name
-}
+# data "aws_eks_cluster" "eks_cluster" {
+#   name = module.eks.cluster_name
+# }
+
+# provider "helm" {
+#   kubernetes {
+#     host                   = data.aws_eks_cluster.eks_cluster.endpoint
+#     cluster_ca_certificate = base64decode("${data.aws_eks_cluster.eks_cluster.certificate_authority[0].data}")
+#     exec {
+#       api_version = "client.authentication.k8s.io/v1beta1"
+#       args        = ["eks", "get-token", "--cluster-name", "${data.aws_eks_cluster.eks_cluster.id}"]
+#       command     = "aws"
+#     }
+#   }
+# }
 
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.eks_cluster.endpoint
-    cluster_ca_certificate = base64decode("${data.aws_eks_cluster.eks_cluster.certificate_authority[0].data}")
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode("${module.eks.cluster_certificate_authority_data}")
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", "${data.aws_eks_cluster.eks_cluster.id}"]
+      args        = ["eks", "get-token", "--cluster-name", "${module.eks.cluster_name}"]
       command     = "aws"
     }
   }

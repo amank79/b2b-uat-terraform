8db093cd6a3713c4ac5ab0a196f8e69eb97f926c
Himalaya7087 <himalayasingh.hs@gmail.com>
2024-12-28 13:19:01 +0530

    modify files

---
+++ cluster-autoscaler.tf
@@ -0,0 +1,16 @@
+# Create IRSA for AWS Cluster Autoscaler
+module "load_balancer_controller_irsa_role" {
+  source                                 = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version                                = "5.42.0"
+  role_name                              = "cluster-autoscaler-${var.environment}"
+  attach_cluster_autoscaler_policy       = true
+  oidc_providers = {
+    eks-cluster = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:cluster-autoscaler"]
+    }
+  }
+  tags = {
+    Name = "ClusterAutoscalerIAMPolicy"
+  }
+}
